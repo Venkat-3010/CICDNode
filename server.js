41366bf77822cef2aac978e27352d5262bc167e0
Venkatesan <akashvenkat47@gmail.com>
2025-12-23 11:52:58 +0530

    fix: update server to listen on dynamic port from environment variable

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/health', (req, res) => {
     });
 });
 
-app.listen(5000, '0.0.0.0', () => {
-    console.log('Server is running on port 5000');
+app.listen(process.env.PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
